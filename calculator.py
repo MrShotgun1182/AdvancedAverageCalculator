8636222408e10173de9688b7132f0b623d0a5dd6
Sameeowrai <Sameeowrai@gmail.com>
2025-01-28 02:51:40 +0330

    Create remove method

--- calculator.py
+++ calculator.py
@@ -7,3 +7,8 @@ class Calculator:
     def add(self,data):
         self.data.append(data)
 
+    def remove(self,name):
+        for data in self.data: 
+            if(data['name'] == name):
+                self.data.remove(data)
+    
